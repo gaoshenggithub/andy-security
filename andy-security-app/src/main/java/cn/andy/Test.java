28b92da41c3803c035572b443d6bec76f80e5618
gaosheng_global <gaosheng_global@andy.com>
2019-09-21 08:21:03 +0800

    初始化

---
+++ andy-security-app/src/main/java/cn/andy/Test.java
@@ -0,0 +1,4 @@
+package cn.andy;
+
+public class Test {
+}
--- andy-security-browser/src/main/java/cn/andy/BrowserSecurityConfig.java
+++ andy-security-browser/src/main/java/cn/andy/BrowserSecurityConfig.java
@@ -1,5 +1,7 @@
 package cn.andy;
 
+import cn.andy.auth.AndyAuthenticationFailureHandler;
+import cn.andy.auth.AndyAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,12 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired
+    private AndyAuthenticationSuccessHandler andyAuthenticationSuccessHandler;
+
+    @Autowired
+    private AndyAuthenticationFailureHandler andyAuthenticationFailureHandler;
+
     @Bean //可以自定义,实现PasswordEncoder
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -27,6 +35,8 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
                 formLogin().
                 loginPage("/auth/require").
                 loginProcessingUrl("/auth/form").
+                successHandler(andyAuthenticationSuccessHandler).
+                failureHandler(andyAuthenticationFailureHandler).
                 and().
                 authorizeRequests().antMatchers("/me","/code/image","/auth/require",securityProperties.getBrowser().getLoginPage()).permitAll().
                 anyRequest().authenticated().and().csrf().disable();
